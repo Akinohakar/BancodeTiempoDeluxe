70d882b25591746f878901fd8b25dd35fcfaa5ca
Alan Aquino <aeaquinorosas@hotmail.com>
2021-09-17 21:43:31 -0500

    version Tomas

--- app/src/main/java/com/example/bancodetiempodeluxe/MainMenu.java
+++ app/src/main/java/com/example/bancodetiempodeluxe/MainMenu.java
@@ -11,66 +11,80 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
+   private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_menu);
-//Gets
-        drawerLayout=findViewById(R.id.drawerLayout);
-        navigationView=findViewById(R.id.navView);
-        toolbar=findViewById(R.id.toolBar);
-//Navigation Drawer Menu
-        setSupportActionBar(toolbar);
-        //Hide or show items
-        Menu menu=navigationView.getMenu();
-        //menu.findItem(R.id.navBuscar).setVisible(false);//para hacer no notar un navigation item
 
-        navigationView.bringToFront();
-        ActionBarDrawerToggle toogle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_closed);
-        drawerLayout.addDrawerListener(toogle);
-        toogle.syncState();
+        Toolbar toolbar=findViewById(R.id.toolabar);
+        setSupportActionBar(toolbar);
+        drawer=findViewById(R.id.drawerLayout);
+        NavigationView navigationView=findViewById(R.id.navView);
         navigationView.setNavigationItemSelectedListener(this);
 
-        navigationView.setCheckedItem(R.id.navPerfil);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_closed);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        if(savedInstanceState==null){//to safe the state when the state is rotated
+        getSupportFragmentManager().beginTransaction().replace(R.id.framentContainer,new fragmentSearch()).commit();//cUNDO SE INICIA LA ACTIVIDAD SE QUIRE ABIERTO EL PRIMERO
+        navigationView.setCheckedItem(R.id.navBuscar);}
 
     }
-//para que no se salga de la app el navegation drawer
+
     @Override
-    public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {//
+        switch(item.getItemId()){
+            case R.id.navBuscar:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framentContainer,new fragmentSearch()).commit();
+                break;
+            case R.id.navTransac:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framentContainer,new transactionsFragment()).commit();
+                break;
+            case R.id.navMensajes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framentContainer,new messageFragment()).commit();
+                break;
+            case R.id.navNotificaciones:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framentContainer,new notificationsFragment()).commit();
+                break;
+            case R.id.navPerfil:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framentContainer,new profileFragment()).commit();
+                break;
+            case R.id.navconfiPerfil:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framentContainer,new configPerfilFragment()).commit();
+                break;
+            case R.id.navWho:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framentContainer,new whoFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public void goProfile(View view){//method to go profile from header image
+        NavigationView navigationView=findViewById(R.id.navView);
+        getSupportFragmentManager().beginTransaction().replace(R.id.framentContainer,new profileFragment()).commit();
+        navigationView.setCheckedItem(R.id.navPerfil);
+        drawer.closeDrawer(GravityCompat.START);
+
+    }
+
+    @Override
+    public void onBackPressed() {//Post Accion of clicked item in navigation drawer
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
         }else{
             super.onBackPressed();
         }
 
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {//When some item is selected
-        switch (item.getItemId()){
-            case R.id.navBuscar:
-                break;//because we already are in there
-            case R.id.navTransac:
-                Intent intent=new Intent(MainMenu.this,Trasactions.class);
-                startActivity(intent);
-                break;
-            case R.id.navPerfil:
-                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
-                break;
-
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
