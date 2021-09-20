94e963e1624473eb6a7356ff0ccb14cf3ea3c5a4
Alan Aquino <aeaquinorosas@hotmail.com>
2021-09-19 19:37:36 -0500

    version Tinajero 4.0

---
+++ app/src/main/java/com/example/bancodetiempodeluxe/offerDescription.java
@@ -0,0 +1,14 @@
+package com.example.bancodetiempodeluxe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class offerDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer_description);
+    }
+}
