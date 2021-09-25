3029321eba0bcd3b2c4f52dd6bd2e4787884e345
Tomas P <jediulises@yahoo.com.mx>
2021-09-25 00:53:22 -0500

    Initial PDF Tests

--- app/src/main/java/com/example/bancodetiempodeluxe/transactionsFragment.java
+++ app/src/main/java/com/example/bancodetiempodeluxe/transactionsFragment.java
@@ -1,18 +1,36 @@
 package com.example.bancodetiempodeluxe;
 
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +41,10 @@ public class transactionsFragment extends Fragment {
 
     ArrayList<TransaccionesModel> listDatos;
     RecyclerView recycler;
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    Bitmap bmp, scaledbmp;
+    String documentTitle = "TestPDF.pdf";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -78,6 +100,15 @@ public class transactionsFragment extends Fragment {
         recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
         listDatos = new ArrayList<TransaccionesModel>();
 
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
+        scaledbmp = Bitmap.createScaledBitmap(bmp, 140, 140,false);
+
+        if(checkPermision()){
+            Toast.makeText(getActivity(), "Permiso Concedido", Toast.LENGTH_SHORT).show();
+        }else{
+            requestPermision();
+        }
+
         //Crear las transacciones
         listDatos.add(new TransaccionesModel("2021-09-24","01","Good Co","Música","Completado","Contrato"));
         listDatos.add(new TransaccionesModel("2021-09-23","01","Emma Clair","Remix","Completado","Contratade"));
@@ -93,7 +124,8 @@ public class transactionsFragment extends Fragment {
         btnPDF.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View view) {
-              generatePDF_transactions();
+              //generatePDF_transactions();
+              create_PDFDocument();
           }
         });
 
@@ -104,6 +136,88 @@ public class transactionsFragment extends Fragment {
         String msg = "Generando Reporte PDF";
         Toast toast = Toast.makeText(getActivity(), msg,Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public void create_PDFDocument(){
+        //Medidas de la pagina
+        int pageHeight = 1120;
+        int pageWidth  = 792;
+
+        //for drawing shapes we use title
+        //for adding text in PDF file
+        Paint paint = new Paint();
+        Paint title = new Paint();
+
+        //Crear nuevo documento
+        PdfDocument document = new PdfDocument();
+
+        //Create a page description
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(pageWidth,pageHeight,1).create();
+
+        //Start a page
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+
+        //Dibujar el logo
+        canvas.drawBitmap(scaledbmp, 56,40, paint);
+
+        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+        title.setTextSize(20);
+
+        title.setColor(ContextCompat.getColor(getActivity(), R.color.blackapp));
+
+        //Parametros: texto, position from start, position from top, paint variable
+        canvas.drawText("Banco de tiempo",209,80,title);
+        canvas.drawText("Reporte Automático Generado", 209,100, title);
+
+        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+        title.setTextSize(15);
+
+        title.setColor(ContextCompat.getColor(getActivity(), R.color.blackapp));
+        canvas.drawText("Listado de Transacciones", 209, 250, title);
+
+        document.finishPage(page);
+
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),documentTitle);
+
+        try{
+            document.writeTo(new FileOutputStream(file));
+            Toast.makeText(getActivity(),"PDF generado Correctamente en "+Environment.getExternalStorageDirectory().getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        document.close();
+    }
+
+    private boolean checkPermision(){
+        int permission1 = ContextCompat.checkSelfPermission(getActivity(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getActivity(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermision(){
+        ActivityCompat.requestPermissions(getActivity(),new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permission, @NonNull int[] grantResults){
+        if(requestCode == PERMISSION_REQUEST_CODE){
+            if (grantResults.length > 0){
+                boolean wrtieStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage  = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if(wrtieStorage && readStorage){
+                    Toast.makeText(getActivity(), "Permiso Concedido", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getActivity(),"Permiso Rechazado", Toast.LENGTH_SHORT).show();
+                    getActivity().finish();
+                }
+            }
+        }
     }
 
 }
