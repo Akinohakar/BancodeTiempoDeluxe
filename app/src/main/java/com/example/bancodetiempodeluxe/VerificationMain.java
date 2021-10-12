f499b40767fff575e96915d052f1f4fc4d608219
Tomas P <jediulises@yahoo.com.mx>
2021-10-12 05:10:03 -0500

    minor changes

--- app/src/main/java/com/example/bancodetiempodeluxe/VerificationMain.java
+++ app/src/main/java/com/example/bancodetiempodeluxe/VerificationMain.java
@@ -2,8 +2,14 @@ package com.example.bancodetiempodeluxe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.getmati.mati_sdk.MatiButton;
+import com.getmati.mati_sdk.MatiSdk;
+import com.getmati.mati_sdk.Metadata;
 
 public class VerificationMain extends AppCompatActivity {
 
@@ -12,5 +18,29 @@ public class VerificationMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        //TODO
+        //Credenciales a obtener de Mati
+        this.<MatiButton>findViewById(R.id.matiKYCButton).setParams(
+                this,
+                "CLIENT_ID",
+                "FLOW_ID",
+                new Metadata.Builder().build());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == MatiSdk.REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText( this,"Verificacion Completada!!!", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(this, MainMenu.class));
+
+            } else {
+                Toast.makeText( this,"Error en la verificación, por favor checa tus documentos", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
